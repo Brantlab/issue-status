17ea6c0966f19a339fa522d5a3fa85ed762c5678
Brantlab <Justin@brantlab.com>
2021-05-13 17:15:20 +0000

    Deploying to gh-pages from @ Brantlab/issue-status@f22464a26588127509a23973ec3304c84d34ecca 🚀

---
+++ precache-manifest.b9d23014bdd159753dfc1dc456eb8512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c759e142d539a12a274a910605b620",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10c6286fbc9d6c57c00f",
+    "url": "./static/css/main.b427710c.chunk.css"
+  },
+  {
+    "revision": "8ee322b3b462f1b65d51",
+    "url": "./static/js/2.3a5a1b98.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "./static/js/2.3a5a1b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c6286fbc9d6c57c00f",
+    "url": "./static/js/main.932a53b6.chunk.js"
+  },
+  {
+    "revision": "dd7b323c43b450bc42d7",
+    "url": "./static/js/runtime-main.237a64a8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4e9ac9f65557f2eeae7ccde9e6217d60.js"
+  "./precache-manifest.b9d23014bdd159753dfc1dc456eb8512.js"
 );
 
 self.addEventListener('message', (event) => {
